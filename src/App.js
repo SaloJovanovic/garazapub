0fd26194a69b153f51e5d6be00b4f4c762a0b961
aleksandar <aleksandarjovanovic670@gmail.com>
2026-09-25 21:22:08 +0200

    izbrisan after work

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Menu from './components/Menu/Menu';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from './components/Footer/Footer';
 import LandingPage from './components/LandingPage/LandingPage';
-import AfterWork from './components/AfterWork/AfterWork';
 
 function App() {
   return (
